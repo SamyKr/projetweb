72f0c93192f16017ca077c105bee98bcf3603b6c
MaelQllt <mael.quillat@orange.fr>
2024-11-24 15:46:08 +0100

    ?

--- projet_web/views/register.php
+++ projet_web/views/register.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Créer un compte</title>
+    <link rel="stylesheet" href="../assets/style_menu.css">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
